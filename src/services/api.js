d7b9d3b575e83d99dfd080cd0257bbd640074858
unknown <thedevicelab8@gmail.com>
2026-05-15 09:54:13 -0600

    session 1 end

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://client-portal-backend-8buf.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
